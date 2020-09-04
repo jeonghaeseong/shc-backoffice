48a2b412bf95f7726cc0ad65aa741de381458b97
Jeong Hae Seong <briskly0415@gmail.com>
2020-09-04 15:36:41 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={{
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/" component={MainPage} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const layout = {
+    labelCol: {
+        span: 6,
+    },
+    wrapperCol: {
+        span: 16,
+    },
+};
+
+const tailLayout = {
+    wrapperCol: {
+        offset: 6,
+        span: 16,
+    },
+};
+
+const LoginPage = () => {
+    const [redirect, setRedirect] = useState(false);
+
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+
+        // axios.get('http://localhost:4000/users').then((response) => {
+        //     console.log(response);
+        //     setRedirect(true);
+        // });
+
+        axios
+            .post('http://localhost:4000/login', {
+                email: values.email,
+                password: values.password,
+            })
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+    if (redirect) {
+        return <Redirect to="/" />;
+    }
+
+    return (
+        <div
+            style={{
+                width: '400px',
+                // marginLeft: 'auto',
+                // marginRight: 'auto',
+                // marginTop: '200px',
+            }}
+        >
+            <Form {...layout} onFinish={onFinish}>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email.',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password.',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    {...tailLayout}
+                    name="remember"
+                    valuePropName="checked"
+                >
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+};
+
+export default LoginPage;
